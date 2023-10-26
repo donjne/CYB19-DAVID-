0b9df00861e299acee3a6e9b5a1c87e0859b1617
donjne <davidjrn247@gmail.com>
2023-10-26 11:38:12 +0100

    complete

---
+++ CYB19.java
@@ -0,0 +1,29 @@
+//NAME: NWANKWOALA DAVID ENYIOMA
+//REG NO: 20211261662
+//DEPARTMENT: CYBERSECURITY
+//COURSE CODE: CSC 201
+//GROUP: 19
+//SERIAL NO: 57
+
+import javax.swing.JOptionPane;
+
+public class CYB19 {
+    public static void main(String[] args) {
+        float z, z1, c = 0.0f;
+        System.out.println("S/N \tNumbers \tsquare \tcube \n");
+        for (int k = 1; k <= 9; k++) {
+            c = Float.parseFloat(JOptionPane.showInputDialog("Enter a number", "0"));
+            z = square(c);
+            z1 = cube(c);
+            System.out.println(k + "\t" + c + "\t" + square(c) + "\t" + z1);
+        }
+    }
+
+    static float square(float x) {
+        return x * x;
+    }
+
+    static float cube(float x) {
+        return x * x * x;
+   }
+}
